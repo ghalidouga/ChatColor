29cdd8eeaf6d12f369ca3d5ffc9c7e2b94e79c4e
ghalidouga <ghalighalidouga@gmail.com>
2020-01-09 15:30:25 +0700

    nambah gui

--- src/main/java/Commands/CColor.java
+++ src/main/java/Commands/CColor.java
@@ -1,19 +1,31 @@
 package Commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class CColor implements CommandExecutor {
 
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(command.getName().equals("chatcolor")){
+        if(command.getName().equals("chatcolor")){ //command /chatcolor
             Player player = (Player) sender;
-            player.sendMessage("test");
-            player.setHealth(10);
+            Inventory CCgui = Bukkit.createInventory(player, 9, "ChatColor"); // gui untuk chat color
+            ItemStack red = new ItemStack(Material.RED_CARPET); // list item di gui
+            ItemStack blue = new ItemStack(Material.BLUE_CARPET);
+            ItemStack yellow = new ItemStack(Material.YELLOW_CARPET);
+
+            ItemStack[] color_items = {red, blue, yellow}; //array
+            CCgui.setContents(color_items); // masukin array ke gui
+            player.openInventory(CCgui); // nampilin gui ke player
         }
 
 
