eaf8c9bcb62b352850c6ada47063ae9dd95ac6b7
ghalidouga <ghalighalidouga@gmail.com>
2020-01-09 14:55:42 +0700

    first commit

---
+++ src/main/java/ghaliwang/chatcolor/ChatColor.java
@@ -0,0 +1,17 @@
+package ghaliwang.chatcolor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatColor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
